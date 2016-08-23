4d0c1869dd1fddc27e3767a912957749111dc905
Jason Mace <jmace01@gmail.com>
2016-08-23 02:05:15 -0600

    Setting pi4j platform

--- src/com/jmace/MaceDrone/ws/context/Context.java
+++ src/com/jmace/MaceDrone/ws/context/Context.java
@@ -1,6 +1,9 @@
 package com.jmace.MaceDrone.ws.context;
 
 import com.jmace.MaceDrone.services.CheckingService;
+import com.pi4j.platform.Platform;
+import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.platform.PlatformManager;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -13,6 +16,11 @@ public class Context implements ServletContextListener {
 	 */
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
+		try {
+			PlatformManager.setPlatform(Platform.RASPBERRYPI);
+		} catch (PlatformAlreadyAssignedException e) {
+			e.printStackTrace();
+		}
 		this.statusThread = new CheckingService();
 		this.statusThread.start();
 	}
